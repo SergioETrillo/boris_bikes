35df936cc12f61b033f6fedcb7740762c0071798
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-04-11 19:13:25 +0100

    add 10: Making Docking Stations Release Bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,23 @@
 require "docking_station"
+require "bike"
 
 describe DockingStation do  	
+	before(:all) do
+		@ds = DockingStation.new
+		@bike = @ds.release_bike
+	end
 	it "responds to release_bike" do 
-		expect(subject).to respond_to(:release_bike) 
+		expect(@ds).to respond_to(:release_bike) 
+	end
+
+	it "gets a bike" do
+		
+		expect(@bike).to be_an_instance_of(Bike)
+		#expect(actual).to be_an_instance_of(expected)
+	end
+
+	it "the bike is working" do
+		expect(@bike.working?).to be true
 	end
 
 	# one liner syntax:
