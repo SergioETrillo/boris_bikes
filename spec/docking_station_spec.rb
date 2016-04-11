c6d63f635aee9d772f6270327522549e5845dff5
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-04-11 19:25:27 +0100

    10 : add 'official' answer to compare

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,24 @@
 require "docking_station"
 require "bike"
 
+=begin
+describe DockingStation do  	
+	# one liner syntax:
+	it { is_expected.to respond_to :release_bike }
+
+
+	it "releases working bikes" do
+		bike = subject.release_bike
+		expect(bike).to be_working
+		#expect(actual).to be_an_instance_of(expected)
+	end
+
+
+		
+end
+
+=end
+
 describe DockingStation do  	
 	before(:all) do
 		@ds = DockingStation.new
@@ -10,14 +28,11 @@ describe DockingStation do
 		expect(@ds).to respond_to(:release_bike) 
 	end
 
-	it "gets a bike" do
+	it "releases a working bike" do
 		
 		expect(@bike).to be_an_instance_of(Bike)
-		#expect(actual).to be_an_instance_of(expected)
-	end
-
-	it "the bike is working" do
 		expect(@bike.working?).to be true
+		#expect(actual).to be_an_instance_of(expected)
 	end
 
 	# one liner syntax:
