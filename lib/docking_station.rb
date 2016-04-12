de8b9674d126542adbe62f6adfc506e1061963c7
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-04-12 16:05:53 +0100

    add 12 raising errors - completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,10 +3,9 @@ class DockingStation
 	attr_reader :bike
 
 	def release_bike
-		p @bike
-		if @bike == nil
-			raise "StandardError"
-		end
+		#p @bike
+		raise "a" if @bike == nil
+			 
 		@bike = Bike.new
 	end
 
@@ -15,4 +14,7 @@ class DockingStation
 	end
 end
 
-d = DockingStation
+#d = DockingStation.new
+#d.dock_bike(Bike.new)
+#d.release_bike
+
