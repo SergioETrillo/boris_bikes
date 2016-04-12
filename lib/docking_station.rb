4e3ec5aa56beec15d8a665f544efc69b697ee687
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-04-12 19:32:03 +0100

    refactor 13 - Capacity

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,22 +5,14 @@ class DockingStation
 	attr_reader :bike
 
 	def release_bike
-		#p @bike
-		raise "no bikes" if @bike == nil	 
+		raise "no bikes" unless @bike 
 		@bike = Bike.new
 	end
 
 	def dock(bike)
 		raise "Over-Capacity" if @bike
 		@bike = bike
-		p bike
 	end
 end
 
-# d = DockingStation.new
-# p d.bike
-# d.dock_bike(Bike.new)
-# d.dock_bike(Bike.new)
-# d.dock_bike(Bike.new)
-# d.dock_bike(Bike.new)
 
