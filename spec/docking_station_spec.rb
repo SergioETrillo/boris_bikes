ae0244fd1d827479ee74950c771dae1ac2a750cb
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-04-13 17:55:40 +0100

    refactor step 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,13 +2,14 @@ require "docking_station"
 
 describe DockingStation do 
 
+	let(:bike) { double(:bike) }
+	let(:broken_bike) { double(:broken_bike) }
+
 	subject(:ds) {DockingStation.new}
 
 	it { is_expected.to respond_to :release_bike }
 
 	context "#release_bike" do	
-		let(:bike) { double(:bike) }
-		let(:broken_bike) { double(:broken_bike) }
 
 		it "releases working bikes" do
 			ds.dock(bike)
@@ -20,8 +21,6 @@ describe DockingStation do
 		it "does not release broken bikes" do
 			allow(broken_bike).to receive(:report_broken).and_return(true)
 			allow(broken_bike).to receive(:broken?).and_return(true)
-			#broken_bike.report_broken
-			#allow(broken_bike).to receive(:dock).and_return(true)
 			ds.dock(broken_bike)
 			expect{ds.release_bike}.to raise_error(RuntimeError, "no working bikes available")
 		end
@@ -29,36 +28,27 @@ describe DockingStation do
 	
 	context "#dock" do
 
-		let(:bike) { double(:bike) }
-		let(:broken_bike) { double(:broken_bike) }
-
 		it "responds to dock with 1 argument" do
 			expect(subject).to respond_to(:dock).with(1).argument 
 		end
 	  
 		it "docks a bike" do
-			#double(:bike)
 			expect(subject.dock(bike)).to eq bike
 		end
 		
 		it "docks a broken bike" do
-			#double(:broken_bike)
 			allow(broken_bike).to receive(:report_broken).and_return(true)
-			#broken_bike.report_broken
 			expect(subject.dock(broken_bike)).to eq broken_bike
 		end
 	end
 	
 	context "test exceptions" do
 
-		let(:bike) { double(:bike) }
-
 		it 'raises error when releasing a bike with zero bikes' do
 			expect{subject.release_bike}.to raise_error(RuntimeError, "no bikes")
 		end
 	
 		it 'raises error when docking on a full station' do 
-			#double(:bike)
 			ds.capacity.times { subject.dock(bike) }
 			expect{subject.dock(bike)}.to raise_error(RuntimeError, "Over-Capacity")
 		end
@@ -74,27 +64,3 @@ describe DockingStation do
 
 	end
 end
-
-
-=begin
-describe DockingStation do  	
-	before(:all) do
-		@ds = DockingStation.new
-		@bike = @ds.release_bike
-	end
-	it "responds to release_bike" do 
-		expect(@ds).to respond_to(:release_bike) 
-	end
-
-	it "releases a working bike" do
-		
-		expect(@bike).to be_an_instance_of(Bike)
-		expect(@bike.working?).to be true
-		#expect(actual).to be_an_instance_of(expected)
-	end
-
-	# one liner syntax:
-	# it { is_expected.to respond_to :release_bike }
-		
-end
-=end
