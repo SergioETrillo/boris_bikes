3a768f021b86626bb379d28e711f616a47bd6936
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-04-15 16:33:20 +0100

    in progress step 21 2nd story

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,26 +1,28 @@
 class Garage
 
-	attr_accessor :bikes
+	attr_accessor :broken_bikes
+  attr_reader :working_bikes
 
 	def initialize
-		@bikes = []
+		@broken_bikes = []
+    @working_bikes = []
 	end
 
 	def store(van)
-		@bikes += van.unload
+		@broken_bikes += van.unload
 	end
 
   def fix_bikes
-    bikes.each {|b|b.fix}
+    broken_bikes.each {|b|b.fix}
   end
 
   def select_working_bikes(remaining_capacity)
-  	if bikes.length <= remaining_capacity
-			delivery = bikes
+  	if working_bikes.length <= remaining_capacity
+			delivery = working_bikes
 		else
-			delivery = bikes.first(remaining_capacity)
+			delivery = working_bikes.first(remaining_capacity)
 		end
-		@bikes -= delivery
+		@working_bikes -= delivery
 		delivery
   end
 end
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -16,16 +16,20 @@ describe Garage do
 
   context 'when creating a new garage' do
 
-  	it 'has no bikes' do
-  		expect(subject.bikes).to be_empty
+  	it 'has no broken bikes' do
+  		expect(subject.broken_bikes).to be_empty
   	end
+
+    it 'has no working bikes' do
+      expect(subject.working_bikes).to be_empty
+    end
   end
 
 	context "dealing with vans" do
 
 		it 'stores broken bikes from a van' do
 			subject.store(van)
-			expect(subject.bikes).to include *(van.broken_bikes)
+			expect(subject.broken_bikes).to include *(van.broken_bikes)
 		end
 
 
@@ -33,21 +37,31 @@ describe Garage do
 
   context "when fixing the bike" do
 
-    it 'calls Bike#fix on each of the bikes in the storage' do
+    it 'calls Bike#fix on each of the broken bikes in the storage' do
       20.times do
         bike = Bike.new
         bike.report_broken
-        subject.bikes << bike
+        subject.broken_bikes << bike
       end
       subject.fix_bikes
       all_fixed = subject.bikes.none? {|b|b.broken?}
       expect(all_fixed).to be true
     end
+
+    it 'empties the broken_bikes buffer' do
+      20.times do
+        bike = Bike.new
+        bike.report_broken
+        subject.broken_bikes << bike
+      end
+      subject.fix_bikes
+      expect(subject.broken_bikes).to be_empty
+    end
   end
 
   context "#select_working_bikes" do
 
-		context "when there are fewer or equal number of broken bikes than remaining capacity" do
+		context "when there are fewer or equal number of working bikes than remaining capacity" do
 
 			it "releases a working bike" do
 				garage.bikes << bike
@@ -64,9 +78,7 @@ describe Garage do
 		context "when there are more broken bikes than the remaining capacity of van" do
 
 			xit "only release bikes until the van is full" do
-				ds.dock(broken_bike)
-				ds.dock(broken_bike2)
-				ds.dock(broken_bike3)
+
 				expect(ds.select_broken_bikes(van.remaining_capacity)).to eq [broken_bike,broken_bike2]
 			end
 
@@ -79,6 +91,4 @@ describe Garage do
 			end
 		end
 	end
-
-
 end
